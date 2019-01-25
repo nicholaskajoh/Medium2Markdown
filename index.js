85549f8a3dc9f4befbaebf35435c3294d53684b2
Nicholas Kajoh <kajohterna@gmail.com>
2019-01-26 00:02:39 +0100

    extract post slugs, remove style tags, comment code

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+// import the File System and Turndown modules
 const fs = require('fs');
 const TurndownService = require('turndown');
+// import helper module util.js
+const util = require('./util');
 
+// use the minimalist package to retrieve a post's path from CLI
+// i.e node index.js --filePath /path/to/medium/post.html
 const argv = require('minimist')(process.argv.slice(2));
 const { filePath } = argv;
 
-const htmlString = fs.readFileSync(filePath, 'utf8');
+// read and clean the contents of the file
+let htmlString = fs.readFileSync(filePath, 'utf8');
+htmlString = util.removeStyleTags(htmlString);
 
+// initialize the Turndown service and convert the HTML to Markdown
 const turndownService = new TurndownService();
 const mdContent = turndownService.turndown(htmlString);
 
-const outputFileName = `./output${filePath.split('.')[1]}.md`;
+// create an output file containing the Markdown
+let postSlug = util.getPostSlug(filePath);
+const outputFileName = `./output/${postSlug}.md`;
 fs.writeFileSync(outputFileName, mdContent);
---
+++ util.js
@@ -0,0 +1,16 @@
+module.exports = {
+  getPostSlug(filePath) {
+    // extract a post's slug from its file path
+    // sample file path: ./input/2017-02-26_Hello-world-1bc5dea68f84.html
+    // post slug: hello-world
+    let postSlug = filePath.replace('./input/', '').replace('.html', '');
+    postSlug = postSlug.replace(/(\d{4})-(\d{1,2})-(\d{1,2})_/, '');
+    postSlug = postSlug.split('-');
+    postSlug.pop();
+    postSlug = postSlug.join('-').toLowerCase().replace(/-+/g, '-');
+    return postSlug;
+  },
+  removeStyleTags(htmlString) {
+    return htmlString.replace(/<style([\S\s]*?)>([\S\s]*?)<\/style>/i, '');
+  },
+}
